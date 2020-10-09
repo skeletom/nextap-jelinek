9b920f87ef7b9bd922d9d3fc17b0a6c1230dd9dc
Tomas Jelinek <email@tomjelinek.cz>
2020-10-09 10:25:57 +0200

    Initial Commit

---
+++ nextap-jelinek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nextap-jelinek
+//
+//  Created by Tomas Jelinek on 09/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
